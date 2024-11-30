57ad73436ab9b2846678f094914bda86120548e5
AbdulrhmanAwadh <abdulrhmanawadh1@gmail.com>
2024-12-01 02:52:21 +0300

    Update accounts.py

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,30 @@
 
 class User(object):
-    def __init__(self, first_name, last_name, email):
-        pass
+     def __repr__(self):
+         return '[<User: "{} {}">]'.format(self.first_name, self.last_name)
 
-    def add_post(self, post):
-        pass
+     def __init__(self, first_name, last_name, email):
+         self.first_name=first_name
+         self.last_name=last_name
+         self.email=email
 
-    def get_timeline(self):
-        pass
+     def add_post(self, post):
+         post.set_user(self)
+         if hasattr(self, 'posts'):
+             self.posts.append(post)
+         else:
+             self.posts=[]
+             self.posts.append(post)
 
-    def follow(self, other):
-        pass
+
+     def get_timeline(self):    
+         timeline_list=[n for x in self.following if hasattr(x, 'posts')==True for n in x.posts]
+         timeline_list.sort(key=lambda x: x.timestamp, reverse=True)
+         return timeline_list
+
+     def follow(self, other):
+         if hasattr(self, 'following'):
+             self.following.append(other)
+         else:
+             self.following=[]
+             self.following.append(other)
